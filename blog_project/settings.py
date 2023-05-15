0526f47cac482edef78c42b377583247963bcce4
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-14 22:28:22 -0300

    Falta formulario avatar

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,7 +134,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ...
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+##STATIC_URL = '/static/'
+##STATICFILES_DIRS = [BASE_DIR / "static"]
 # ...
 
